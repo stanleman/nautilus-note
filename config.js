eda0e0a789e2cf4eabf5d49e1975df800fd01b20
stanleman <stanlord2009@gmail.com>
2024-08-26 10:21:49 +0800

    bug fix: added env for deployment

--- config.js
+++ config.js
@@ -12,12 +12,20 @@ const firebaseConfig = {
   // measurementId: "G-KR0CDMC0XK",
 
   // Firestore 2
-  apiKey: "AIzaSyASl8qnpLrJeDLExIDNDVwvqyCBrvIvjXs",
-  authDomain: "nautilus-note-lmao.firebaseapp.com",
-  projectId: "nautilus-note-lmao",
-  storageBucket: "nautilus-note-lmao.appspot.com",
-  messagingSenderId: "503090251112",
-  appId: "1:503090251112:web:527487d09f7e941ff6c2dd",
+  // apiKey: "AIzaSyASl8qnpLrJeDLExIDNDVwvqyCBrvIvjXs",
+  // authDomain: "nautilus-note-lmao.firebaseapp.com",
+  // projectId: "nautilus-note-lmao",
+  // storageBucket: "nautilus-note-lmao.appspot.com",
+  // messagingSenderId: "503090251112",
+  // appId: "1:503090251112:web:527487d09f7e941ff6c2dd",
+
+  // WITH ENV
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
